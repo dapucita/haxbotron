10a857c3c8bd6fd964350d329910331b49ab279f
dapucita <dapucita@gmail.com>
2019-06-30 13:38:24 +0900

    resources file

---
+++ resources/strings.ts
@@ -0,0 +1,5 @@
+export const RStrings = {
+    headlineMessage: "This room is powered by Haxbotron.",
+    joinMessage: `[System] Welcome, {player.name}#{player.id}!`,
+    statsMessage: "[System] You wins {{player.stats.wins}} times of {{player.stats.totals}} games."
+}
