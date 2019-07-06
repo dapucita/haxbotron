367960b3679c02887aa7e11938e613cbd39905c1
dapucita <dapucita@gmail.com>
2019-07-06 16:25:40 +0900

    new game rule as default

---
+++ model/rules/captain.rule.ts
@@ -0,0 +1,19 @@
+import { GameRule } from "../GameRule";
+import * as huge from "../../stadium/huge.hbs";
+import * as miniready from "../../stadium/miniready.hbs";
+
+export var gameRule: GameRule = {
+    ruleName: "captain",
+    ruleVersion: "0.0.1",
+    ruleAuthor: "Haxbotron",
+    ruleDescripttion: "Haxbotron default game rule",
+    requisite: {
+        minimumPlayers: 4,
+        timeLimit: 3,
+        scoreLimit: 3
+    },
+    autoOperating: false,
+    statsRecord: true,
+    defaultMap: huge.stadiumText,
+    readyMap: miniready.stadiumText
+}
