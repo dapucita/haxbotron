4985bcb1d2b64bfa6c5181e6e9beba8297021235
dapucita <dapucita@gmail.com>
2019-07-06 22:18:07 +0900

    store players' data persistly

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
+import { async } from "q";
+
 //BOT Loader
 const readline = require("readline");
 const puppeteer = require('puppeteer');
+const nodeStorage = require('node-persist');
 
 var isOpenHeadless: boolean = false; //option for open chrome in headless mode
 var isBotLaunched: boolean = false;
@@ -9,6 +12,8 @@ var headlessToken: string = '';
 var pageContainer:any;
 
 async function bot() {
+    await nodeStorage.init(); // Set LocalStorage node-sideeStorage.setItem()
+
     const browser = await puppeteer.launch({
         headless: isOpenHeadless,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
@@ -20,6 +25,7 @@ async function bot() {
             console.log(`[PUPPETEER] ${msg.args()[i]}`); //args example: console.log('hello', 5, {foo: 'bar'}) on JS injected in puppeteer
         }
     });*/
+
     await page.goto('https://www.haxball.com/headless', {
         waitUntil: 'networkidle2'
     });
@@ -27,9 +33,58 @@ async function bot() {
     await page.addScriptTag({
         path: './out/bot_bundle.js'
     });
+
+    // load stored datas from node-persist storage to puppeteer html5 localstorage
+    await nodeStorage.forEach(async function (datum: any) { // async forEach(callback): This function iterates over each key/value pair and executes an asynchronous callback as well
+        // usage: datum.key, datum.value
+        await page.evaluate( (tempKey: string, tempStr: string) => {
+            localStorage.setItem(tempKey, tempStr);
+        }, datum.key, datum.value );
+    });
+
+    // get stored datas from puppeteer html5 localstorage and copy it into node-persist storage
+    setInterval(async function (){
+        var localStorageData: any[] = await page.evaluate( () => {
+            let jsonData: any = {};
+            for (let i = 0; i < localStorage.length; i++) {
+                const key: string|null = localStorage.key(i);
+                if(typeof key === "string") {
+                    jsonData[key] = localStorage.getItem(key);
+                }
+            }
+            return jsonData;
+        });
+        Object.keys(localStorageData).forEach(function (elementKey: any) {
+            nodeStorage.setItem(elementKey, localStorageData[elementKey]);
+        });
+    }, 5000); // by each 5seconds
     
     return page;
 }
+
+/*async function saveLocalStorage() {
+    // onto node
+    await pageContainer.evaluate( () => {
+        for (let i = 0; i < localStorage.length; i++) {
+            const key: string|null = localStorage.key(i);
+            if(typeof key === "string") {
+                nodeStorage.setItem(key, localStorage.getItem(key));
+            }
+        }
+    });
+}*/
+
+/*async function loadLocalStorage() {
+    // onto puppeteer
+    for(let i = 0; i < nodeStorage.length(); i++) {
+        const key: string|null = nodeStorage.key(i);
+        if(typeof key === "string") {
+            await pageContainer.evaluate( () => {
+                localStorage.setItem(key, nodeStorage.getItem(key));
+            });
+        }
+    }
+}*/
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -50,7 +105,7 @@ rl.on("line", (inputData: string) => {
     switch (data[0]) {
         case "start": {
             if (isBotLaunched) {
-                console.log("Bot already has been launched.")
+                console.log("Bot already has been launched.");
             } else {
                 if(isTokenEmpty) { // if headless token not setted yet
                     console.log("error: you need to set headless token for starting bot. use token command.");
@@ -63,6 +118,15 @@ rl.on("line", (inputData: string) => {
             }
             break;
         }
+        /*
+        case "watch": {
+            if (isBotLaunched) {
+                setInterval(saveLocalStorage, 1000);
+            } else {
+                console.log("Start bot first for watching localStorage.");
+            }
+            break;
+        }*/
         case "token": {
             if(data[1] === undefined || data[1] == '') {
                 console.log("error: token key field should not be empty.");
@@ -74,6 +138,7 @@ rl.on("line", (inputData: string) => {
             break;
         }
         case "exit": {
+            //saveLocalStorage();
             process.exit(0);
             break;
         }
