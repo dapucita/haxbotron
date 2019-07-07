dd7a329f43046f24a948584e113e86a343311c28
dapucita <dapucita@gmail.com>
2019-07-07 15:58:23 +0900

    new function for calculate assists rate

--- controller/Statistics.ts
+++ controller/Statistics.ts
@@ -1,15 +1,19 @@
-export function calcWinRate(totalGames: number, winGames: number): number {
+export function calcWinsRate(totalGames: number, winGames: number): number {
     // calculate the given Player's winning games rate
     return Math.round((winGames / totalGames) * 100);
 }
 
-export function calcGoalPerGame(totalGames: number, totalGoals: number): number {
+export function calcGoalsPerGame(totalGames: number, totalGoals: number): number {
     // calculate the given Player's goal rate per game
     return Math.round((totalGoals / totalGames) * 100);
 }
 
-export function calcOGPerGame(totalGames: number, totalOGs: number): number {
+export function calcOGsPerGame(totalGames: number, totalOGs: number): number {
     // calculate the given Player's goal rate per game
     return Math.round((totalOGs / totalGames) * 100);
 }
 
+export function calcAssistsPerGame(totalGames: number, totalAssists: number): number {
+    // calculate the given Player's goal rate per game
+    return Math.round((totalAssists / totalGames) * 100);
+}
