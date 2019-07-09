1e6494c4359c073d3ccdddf2ea8542c676341653
dapucita <dapucita@gmail.com>
2019-07-09 21:26:35 +0900

    start developing UI by using Electron

--- index.ts
+++ index.ts
@@ -1,35 +1,116 @@
-import { async } from "q";
+//Electron Loader
+const {
+    app,
+    BrowserWindow,
+    Menu
+} = require('electron');
+const {
+    ipcMain
+} = require('electron');
+const menuTemplate = require('./view/menuTemplate');
 
 //BOT Loader
 const readline = require("readline");
 const puppeteer = require('puppeteer');
 const nodeStorage = require('node-persist');
 
+let electronWindow; //window object for Electron
+
 var isOpenHeadless: boolean = false; //option for open chrome in headless mode
 var isBotLaunched: boolean = false;
 var isTokenEmpty: boolean = true;
 var headlessToken: string = '';
-var pageContainer:any;
+var pageContainer: any;
+
+function createWindow() {
+    // create the Electron browser window
+    electronWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // and load the index.html of the app.
+    electronWindow.loadFile('../index.html');
+
+    // Open the DevTools.
+    //electronWindow.webContents.openDevTools();
+
+    // When the window is closed
+    electronWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        electronWindow = null;
+    });
+}
+
+// Event handler for asynchronous incoming messages
+ipcMain.on('asynchronous-message', (event: any, arg: any) => {
+    console.log(arg);
+
+    /* Event emitter for sending asynchronous messages */
+    event.sender.send('asynchronous-reply', 'async pong');
+});
+
+// Event handler for synchronous incoming messages
+ipcMain.on('synchronous-message', (event: any, arg: any) => {
+    console.log(arg);
+
+    /* Synchronous event emmision in 10 sec */
+    setTimeout(() => {
+        event.returnValue = 'sync pong';
+    }, 10 * 1000);
+
+});
+
+const electronMenu = Menu.buildFromTemplate(menuTemplate);
+
+Menu.setApplicationMenu(electronMenu);
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', (electronWindow: any) => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (electronWindow === null) {
+        createWindow();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process code.
+// You can also put them in separate files and require them here.
 
 async function bot() {
-    await nodeStorage.init(); // Set LocalStorage node-sideeStorage.setItem()
+    await nodeStorage.init();
 
     const browser = await puppeteer.launch({
         headless: isOpenHeadless,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    /*await page.on('console', (msg: any) => {
-        //output messages come from puppeteer console
-        for (let i = 0; i < msg.args().length; ++i) {
-            console.log(`[PUPPETEER] ${msg.args()[i]}`); //args example: console.log('hello', 5, {foo: 'bar'}) on JS injected in puppeteer
-        }
-    });*/
 
     await page.goto('https://www.haxball.com/headless', {
         waitUntil: 'networkidle2'
     });
-    await page.setCookie({name: 'botToken', value: headlessToken}); //auth token for headless-api page
+    await page.setCookie({
+        name: 'botToken',
+        value: headlessToken
+    }); //auth token for headless-api page
     await page.addScriptTag({
         path: './out/bot_bundle.js'
     });
@@ -37,18 +118,18 @@ async function bot() {
     // load stored datas from node-persist storage to puppeteer html5 localstorage
     await nodeStorage.forEach(async function (datum: any) { // async forEach(callback): This function iterates over each key/value pair and executes an asynchronous callback as well
         // usage: datum.key, datum.value
-        await page.evaluate( (tempKey: string, tempStr: string) => {
+        await page.evaluate((tempKey: string, tempStr: string) => {
             localStorage.setItem(tempKey, tempStr);
-        }, datum.key, datum.value );
+        }, datum.key, datum.value);
     });
 
     // get stored datas from puppeteer html5 localstorage and copy it into node-persist storage
-    setInterval(async function (){
-        var localStorageData: any[] = await page.evaluate( () => {
+    setInterval(async function () {
+        var localStorageData: any[] = await page.evaluate(() => {
             let jsonData: any = {};
             for (let i = 0; i < localStorage.length; i++) {
-                const key: string|null = localStorage.key(i);
-                if(typeof key === "string") {
+                const key: string | null = localStorage.key(i);
+                if (typeof key === "string") {
                     jsonData[key] = localStorage.getItem(key);
                 }
             }
@@ -58,33 +139,13 @@ async function bot() {
             nodeStorage.setItem(elementKey, localStorageData[elementKey]);
         });
     }, 5000); // by each 5seconds
-    
+
     return page;
 }
 
-/*async function saveLocalStorage() {
-    // onto node
-    await pageContainer.evaluate( () => {
-        for (let i = 0; i < localStorage.length; i++) {
-            const key: string|null = localStorage.key(i);
-            if(typeof key === "string") {
-                nodeStorage.setItem(key, localStorage.getItem(key));
-            }
-        }
-    });
-}*/
+/*===========
 
-/*async function loadLocalStorage() {
-    // onto puppeteer
-    for(let i = 0; i < nodeStorage.length(); i++) {
-        const key: string|null = nodeStorage.key(i);
-        if(typeof key === "string") {
-            await pageContainer.evaluate( () => {
-                localStorage.setItem(key, nodeStorage.getItem(key));
-            });
-        }
-    }
-}*/
+
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -107,10 +168,9 @@ rl.on("line", (inputData: string) => {
             if (isBotLaunched) {
                 console.log("Bot already has been launched.");
             } else {
-                if(isTokenEmpty) { // if headless token not setted yet
+                if (isTokenEmpty) { // if headless token not setted yet
                     console.log("error: you need to set headless token for starting bot. use token command.");
                 } else {
-                    //bot();
                     pageContainer = bot();
                     isBotLaunched = true;
                     console.log("Bot loaded.");
@@ -118,17 +178,8 @@ rl.on("line", (inputData: string) => {
             }
             break;
         }
-        /*
-        case "watch": {
-            if (isBotLaunched) {
-                setInterval(saveLocalStorage, 1000);
-            } else {
-                console.log("Start bot first for watching localStorage.");
-            }
-            break;
-        }*/
         case "token": {
-            if(data[1] === undefined || data[1] == '') {
+            if (data[1] === undefined || data[1] == '') {
                 console.log("error: token key field should not be empty.");
             } else {
                 console.log(`token key is saved. (${data[1]})`);
@@ -138,29 +189,9 @@ rl.on("line", (inputData: string) => {
             break;
         }
         case "exit": {
-            //saveLocalStorage();
             process.exit(0);
             break;
         }
-        /*
-        case "ping": { // eval test
-            if(isBotLaunched) {
-                //pageContainer.then( ( page:any ) => { page.setCookie({name: 'ping', value: 'ping from main launcher'}); } );
-                pageContainer.then( (page: any) => { page.evaluate(() => { window.ping(); }) } );
-            } else {
-                console.log("error: you can do ping only when the bot is launched.");
-            }
-            break;
-        }
-        case "addping": { // eval test
-            if(isBotLaunched) {
-                pageContainer.then( (page: any) => { page.exposeFunction('ping', () => { console.log("piiing!!"); }) } );
-            } else {
-                console.log("error: you can add ping only when the bot is launched.");
-            }
-            break;
-        } 
-        */
         case "help": {
             console.log("Haxbotron CLI | Available commands : start, exit, token, help");
             break;
@@ -172,9 +203,4 @@ rl.on("line", (inputData: string) => {
     rl.prompt();
 });
 
-/* puppeteer cookie usage
-https://stackoverflow.com/questions/50584770/passing-multiple-cookies-to-puppeteer
-await page.setCookie(...cookies);
-const cookiesSet = await page.cookies(url);
-console.log(JSON.stringify(cookiesSet));
-*/
+===========*/
