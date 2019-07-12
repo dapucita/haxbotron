8084c43d0a04aee290e27baf8ee295f48d7ee010
dapucita <dapucita@gmail.com>
2019-07-12 21:34:10 +0900

    menu add

--- view/menuTemplate.ts
+++ view/menuTemplate.ts
@@ -1,40 +1,75 @@
-const template = [
-    {
-    label: 'Bot',
-    submenu: [
-        { label: 'Start' },
-        { label: 'Stop' },
-        { type: 'separator' },
-        { label: 'Quit' }
-    ]
+const template = [{
+        label: 'Application',
+        submenu: [{
+                id: 'startMenuItem',
+                label: 'Start'
+            },
+            {
+                id: 'stopMenuItem',
+                enabled: false,
+                label: 'Stop'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'quit'
+            }
+        ]
     },
     {
-    label: 'View',
-    submenu: [
-        {role: 'toggledevtools'},
-        {type: 'separator'},
-        {role: 'resetzoom'},
-        {role: 'zoomin'},
-        {role: 'zoomout'},
-        {type: 'separator'},
-        {role: 'togglefullscreen'}
-    ]
+        label: 'View',
+        submenu: [{
+                role: 'toggledevtools'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'resetzoom'
+            },
+            {
+                role: 'zoomin'
+            },
+            {
+                role: 'zoomout'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'togglefullscreen'
+            }
+        ]
     },
     {
-    role: 'window',
-    submenu: [
-        {role: 'minimize'},
-        {role: 'close'}
-    ]
-    },
-    {
-    role: 'help',
-    submenu: [
-        {
-        label: 'Learn More',
-        click () { require('electron').shell.openExternal('https://electronjs.org') }
+        label: 'Setting',
+        submenu: [{
+            id: 'headlessModeMenuItem',
+            type: 'checkbox',
+            checked: false,
+            label: 'headless mode'
         }
-    ]
+        ]
+    },
+    {
+        label: 'Help',
+        submenu: [{
+                label: 'Check for Updates'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Website',
+                click() {
+                    require('electron').shell.openExternal('https://dapucita.github.io/haxbotron/')
+                }
+            },
+            {
+                label: 'About'
+            }
+        ]
     }
 ];
 
