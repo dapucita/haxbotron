6ebb941b78d5d54737d134191d21434343cbb33c
dapucita <dapucita@gmail.com>
2019-07-13 18:22:53 +0900

    remove inoperative menu

--- view/menuTemplate.ts
+++ view/menuTemplate.ts
@@ -28,18 +28,6 @@ const template = [{
                 type: 'separator'
             },
             {
-                role: 'resetzoom'
-            },
-            {
-                role: 'zoomin'
-            },
-            {
-                role: 'zoomout'
-            },
-            {
-                type: 'separator'
-            },
-            {
                 role: 'togglefullscreen'
             }
         ]
