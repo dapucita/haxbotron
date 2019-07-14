b2869518bb091b40e96813fd8699a0f8fbd5e88e
dapucita <dapucita@gmail.com>
2019-07-14 18:35:23 +0900

    add and modify contents

--- resources/strings.ts
+++ resources/strings.ts
@@ -1,3 +1,6 @@
+// YOU CAN USE A PLACEHOLDER FOR INTERPOLATION. FOR EXAMPLE, 'Hello, My name is {name}.'
+// THE TYPES OF PLACEHOLDER ARE LIMITED BY STRING SET.
+
 export const command = {
     help: '📄 !about !stats 📑 !help COMMAND for detail'
     ,helpman: { // detailed description for a command
@@ -7,6 +10,26 @@ export const command = {
         ,stats: '📑 !stats shows all players your statistical information.'
     } 
     ,about: '📄 Haxbotron bot - launched on {{_LaunchTime}}'
-    ,super: '🚨  You are super admin now.'
-    ,debug: '🚨 Debug information has printed in console.'
+    ,super: '👑 You are super admin now.'
+    ,debug: '👑 Debug information has printed in console.'
+}
+
+export const onJoin = {
+    welcome: '📢 Welcome, ${playerName}#${playerID}! 📄 You can get informations by command !help'
+    ,changename: '📢 {playerName}#{playerID} has changed name from {playerNameOld}'
+    ,startRecord: '📊 Enough players has joined, so the game\'s result will be recorded from now.'
+    ,stopRecord: '📊 Need more players. The game\'s result will not be recorded from now. (needs {gameRuleNeedMin} players at least)'
+}
+
+export const onLeft = {
+    startRecord: '📊 Enough players has joined, so the game\'s result will be recorded from now.'
+    ,stopRecord: '📊 Need more players. The game\'s result will not be recorded from now. (needs {gameRuleNeedMin} players at least)'
+}
+
+export const onChat = {
+    mutedChat: '🔇 You are muted. You can\'t send message to others.'
+}
+
+export const onGoal = {
+    goal: '⚽️ '
 }
