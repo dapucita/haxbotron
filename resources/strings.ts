f22a31f81112ebabe84ed8b8f78a77b585f365fa
dapucita <dapucita@gmail.com>
2019-07-14 15:48:19 +0900

    new strings

--- resources/strings.ts
+++ resources/strings.ts
@@ -1,6 +1,12 @@
 export const command = {
-    help: '🧾 !help !about'
-    ,about: '🧾 Haxbotron bot - launched on {{_LaunchTime}}'
-    ,super: '⚙️  You are super admin now.'
-    ,debug: '⚙️ Debug information has printed in console.'
+    help: '📄 !about !stats 📑 !help COMMAND for detail'
+    ,helpman: { // detailed description for a command
+        _ErrorWrongMan : '❌ Failed to read about that command.'
+        ,help: '📑 !help COMMAND shows you how to use COMMAND command.'
+        ,about: '📑 !about shows you simple inforamtion of the bot running now.'
+        ,stats: '📑 !stats shows all players your statistical information.'
+    } 
+    ,about: '📄 Haxbotron bot - launched on {{_LaunchTime}}'
+    ,super: '🚨  You are super admin now.'
+    ,debug: '🚨 Debug information has printed in console.'
 }
