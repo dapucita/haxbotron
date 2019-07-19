c75949accb54dfd11f7ea50a728ed546a69b851a
dapucita <dapucita@gmail.com>
2019-07-20 00:43:39 +0900

    fix to not display NaN when the argument is 0

--- controller/Statistics.ts
+++ controller/Statistics.ts
@@ -1,19 +1,31 @@
 export function calcWinsRate(totalGames: number, winGames: number): number {
     // calculate the given Player's winning games rate
+    if(totalGames == 0 && winGames == 0) {
+        return 0;
+    }
     return Math.round((winGames / totalGames) * 100);
 }
 
 export function calcGoalsPerGame(totalGames: number, totalGoals: number): number {
     // calculate the given Player's goal rate per game
+    if(totalGames == 0 && totalGoals == 0) {
+        return 0;
+    }
     return Math.round((totalGoals / totalGames) * 100);
 }
 
 export function calcOGsPerGame(totalGames: number, totalOGs: number): number {
     // calculate the given Player's goal rate per game
+    if(totalGames == 0 && totalOGs == 0) {
+        return 0;
+    }
     return Math.round((totalOGs / totalGames) * 100);
 }
 
 export function calcAssistsPerGame(totalGames: number, totalAssists: number): number {
     // calculate the given Player's goal rate per game
+    if(totalGames == 0 && totalAssists == 0) {
+        return 0;
+    }
     return Math.round((totalAssists / totalGames) * 100);
 }
