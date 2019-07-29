b5c279ff6512188ec40a273fc63dec2ab90619ba
dapucita <dapucita@gmail.com>
2019-07-29 18:14:36 +0900

    new feature

--- model/RoomConfig.ts
+++ model/RoomConfig.ts
@@ -12,9 +12,15 @@ export interface RoomConfig {
     // GeoLocation override for the room.
     geo?: {
         "code": string,
-        "lat": number,
-        "lon": number
+        "lat": number, // flat
+        "lon": number // flat
     };
     // token doesn't need if the bot is started from Headless page, not standalone.
     token?: string;
+    // If set to true the room player list will be empty, the playerName setting will be ignored.
+    // Default value is false for backwards compatibility reasons but it's recommended to set this to true.
+    noPlayer?: boolean;
 }
+/* 
+Warning! events will have null as the byPlayer argument when the event is caused by the host, so make sure to check for null values!
+*/
