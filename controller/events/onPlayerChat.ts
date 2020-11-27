e02eb6172a1bda20f1494e1dc5f6eec284b46c62
dapucita <dapucita@gmail.com>
2020-11-27 18:49:08 +0900

    bug fix  for anti chat flood

--- controller/events/onPlayerChat.ts
+++ controller/events/onPlayerChat.ts
@@ -14,7 +14,7 @@ export function onPlayerChatListener(player: PlayerObject, message: string): boo
 
     window.logger.i(`${player.name}#${player.id} said, "${message}"`);
 
-    var placeholderChat = { 
+    var placeholderChat = {
         playerID: player.id,
         playerName: player.name,
         gameRuleName: gameRule.ruleName,
@@ -28,42 +28,41 @@ export function onPlayerChatListener(player: PlayerObject, message: string): boo
         streakTeamCount: window.winningStreak.getCount()
     };
 
-    if(BotSettings.antiChatFlood === true && player.admin === false && window.playerList.get(player.id).permissions['mute'] === false) { // if anti chat flood options is enabled (admin player can say anytime)
-        console.log("[DEBUG] antiChatFlood 프로시져");
-        let chatFloodCritFlag: boolean = false;
-        window.antiTrollingChatFloodCount.push(player.id); // record who said this chat
-        for(let floodCritCount = 1; floodCritCount <= BotSettings.chatFloodCriterion; floodCritCount++) {
-            let floodID: number = window.antiTrollingChatFloodCount[window.antiTrollingChatFloodCount.length - floodCritCount] || 0;
-            console.log("[DEBUG] antiChatFlood floodID : "+floodID);
-            if(floodID === player.id) {
-                console.log("[DEBUG] antiChatFlood 플래그 will be true.");
-                chatFloodCritFlag = true;
+    // =========
+
+    if (isCommandString(message) === true) { // if this message is command chat
+        parseCommand(player, message); // evaluate it
+        return false; // and show this message for only him/herself
+    } else { // if this message is normal chat
+        if (player.admin === true) { // if this player is admin
+            return true; // admin can chat regardless of mute
+        } else {
+            if (window.isMuteAll === true || window.playerList.get(player.id).permissions['mute'] === true) { // if this player is muted or whole chat is frozen
+                window.room.sendAnnouncement(Tst.maketext(LangRes.onChat.mutedChat, placeholderChat), player.id, 0xFF0000, "bold", 2); // notify that fact
+                return false; // and hide this chat
             } else {
-                console.log("[DEBUG] antiChatFlood 플래그 will be false.");
-                chatFloodCritFlag = false;
-                break; // abort loop
+                //Anti Chat Flood Checking
+                if (BotSettings.antiChatFlood === true) { // if anti chat flood options is enabled
+                    let chatFloodCritFlag: boolean = false;
+                    window.antiTrollingChatFloodCount.push(player.id); // record who said this chat
+                    for (let floodCritCount = 1; floodCritCount <= BotSettings.chatFloodCriterion; floodCritCount++) {
+                        let floodID: number = window.antiTrollingChatFloodCount[window.antiTrollingChatFloodCount.length - floodCritCount] || 0;
+                        if (floodID === player.id) {
+                            chatFloodCritFlag = true;
+                        } else {
+                            chatFloodCritFlag = false;
+                            break; // abort loop
+                        }
+                    }
+                    if (chatFloodCritFlag === true) { // after complete loop, check flag
+                        // judge as chat flood.
+                        window.playerList.get(player.id).permissions['mute'] = true; // mute this player
+                        window.room.sendAnnouncement(Tst.maketext(LangRes.antitrolling.chatFlood.muteReason, placeholderChat), null, 0xFF0000, "bold", 1); // notify that fact
+                        return false;
+                    }
+                }
+                return true;
             }
         }
-        if(chatFloodCritFlag === true) { // after complete loop, check flag
-            console.log("[DEBUG] antiChatFlood 뮤트 will be.");
-            // judge as chat flood.
-            window.playerList.get(player.id).permissions['mute'] = true; // mute this player
-            window.room.sendAnnouncement(Tst.maketext(LangRes.antitrolling.chatFlood.muteReason, placeholderChat), null, 0xFF0000, "bold", 1); // notify that fact
-            //return false; // ignore the chat message of this turn //DO NOT return false cuz command chat will be working
-        }
-    }
-
-    if (isCommandString(message) == true) { // if the message is command chat
-        parseCommand(player, message); // evaluate it
-        return false; // say only to him/herself
-    } else { // if the message is normal chat
-        if(player.admin == true) { // if the player is admin
-            return true; // admin can chat regardless of mute
-        }
-        if (window.isMuteAll == true || window.playerList.get(player.id).permissions['mute'] == true) { // if the player is muted
-            window.room.sendAnnouncement(Tst.maketext(LangRes.onChat.mutedChat, placeholderChat), player.id, 0xFF0000, "bold", 2); // notify that fact
-            return false; // and filter the chat message to other players.
-        }
-        return true;
     }
 }
