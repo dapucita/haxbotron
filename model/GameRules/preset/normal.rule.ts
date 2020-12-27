f7d8c5a3fc1f72fa9bae85bfb8450cc9bb680df4
dapucita <dapucita@gmail.com>
2020-12-27 20:01:46 +0900

    new game rule

---
+++ model/GameRules/preset/normal.rule.ts
@@ -0,0 +1,24 @@
+import { GameRule } from "../GameRule";
+import * as gbhothuge from "../../../resources/stadium/gbhothuge.hbs";
+import * as gbtraining from "../../../resources/stadium/gbtraining.hbs";
+
+export var gameRule: GameRule = {
+    ruleName: "normal",
+    ruleVersion: "0.0.1",
+    ruleAuthor: "Haxbotron",
+    ruleDescripttion: "Haxbotron default game rule",
+    requisite: {
+        minimumPlayers: 8,
+        maximumTeamLimit: 4,
+        minimumTeamLimit: 4,
+        timeLimit: 3,
+        scoreLimit: 3,
+        teamLock: true
+    },
+    autoAdmin: true,
+    autoOperating: false,
+    captain: false,
+    statsRecord: true,
+    defaultMap: gbhothuge.stadiumText,
+    readyMap: gbtraining.stadiumText
+}
