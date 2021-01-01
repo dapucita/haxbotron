d1d74c59cdb6638a4c30b569e72d4f7523131c84
dapucita <dapucita@gmail.com>
2021-01-01 14:29:31 +0900

    bug fix

--- core/controller/events/onPlayerAdminChange.ts
+++ core/controller/events/onPlayerAdminChange.ts
@@ -7,10 +7,13 @@ import { PlayerObject } from "../../model/GameObject/PlayerObject";
 export function onPlayerAdminChangeListener(changedPlayer: PlayerObject, byPlayer: PlayerObject): void {
     /* Event called when a player's admin rights are changed.
             byPlayer is the player which caused the event (can be null if the event wasn't caused by a player). */
-    var placeholderAdminChange = {
+    let placeholderAdminChange = {
         playerID: changedPlayer.id,
         playerName: changedPlayer.name
     }
+
+    window.playerList.get(changedPlayer.id)!.admin = changedPlayer.admin; // update
+
     if (changedPlayer.admin == true) { // if this event means that the player has been admin
         if (window.playerList.get(changedPlayer.id)!.permissions.afkmode == true) {
             // if changedPlayer is in afk mode, reject
@@ -19,13 +22,13 @@ export function onPlayerAdminChangeListener(changedPlayer: PlayerObject, byPlaye
             return;
         } else {
             // make this player admin
-            window.playerList.get(changedPlayer.id)!.admin = true;
             if (byPlayer !== null) {
                 window.logger.i(`${changedPlayer.name}#${changedPlayer.id} has been admin(super:${window.playerList.get(changedPlayer.id)!.permissions.superadmin}) by ${byPlayer.name}#${byPlayer.id}`);
             }
             return;
         }
     }
+    
     if(window.settings.game.rule.autoAdmin === true) { // if auto admin option is enabled
         updateAdmins(); // check when the last admin player disqulified by self
     }
