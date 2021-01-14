03974d565a70dc611bd36f7858b2a20e434419e2
dapucita <dapucita@gmail.com>
2021-01-14 20:48:30 +0900

    correct typos

--- db/router/v1.superadmin.router.ts
+++ db/router/v1.superadmin.router.ts
@@ -9,19 +9,19 @@ export const superadminRouter: Router = express.Router({ mergeParams: true });
 const superadminRepository: IRepository<SuperAdmin> = new SuperAdminRepository();
 const controller: SuperAdminController = new SuperAdminController(superadminRepository);
 
-// /v1/banlist POST
+// /v1/superadmin POST
 // register new superadmin key
 superadminRouter.post('/', validateSuperAdminModelRules, checkValidationRules, async (request: Request, response: Response, next: NextFunction) => {
     await controller.addSuperAdmin(request, response, next);
 });
 
-// /v1/banlist/:conn GET
+// /v1/superadmin/:key GET
 // get the superadmin data (key and description)
 superadminRouter.get('/:key', async (request: Request, response: Response, next: NextFunction) => {
     await controller.getSuperAdmin(request, response, next);
 });
 
-// /v1/banlist/:conn DELETE
+// /v1/superadmin/:key DELETE
 // delete superadmin key
 superadminRouter.delete('/:key', async (request: Request, response: Response, next: NextFunction) => {
     await controller.deleteSuperAdmin(request, response, next);
