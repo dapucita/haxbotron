be5aa7f5982672f85000ddcaf3966b9909a056c7
dapucita <dapucita@gmail.com>
2021-01-26 23:39:48 +0900

    material ui

--- core/react/component/App.tsx
+++ core/react/component/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
     return (
         <div className="App">
             <h1>Haxbotron Headless Host Server</h1>
+            <Button variant="contained" color="primary">Login!</Button>
         </div>
     );
 }
