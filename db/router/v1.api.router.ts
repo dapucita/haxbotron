48b32c9201bc9fb8432ed4ae6cae2dfc53619f01
dapucita <dapucita@gmail.com>
2021-01-26 16:43:40 +0900

    cors

--- db/router/v1.api.router.ts
+++ db/router/v1.api.router.ts
@@ -2,8 +2,11 @@ import express, { Request, Response, Router, NextFunction } from "express";
 import { playerRouter } from "./v1.player.router";
 import { banlistRouter } from "./v1.banlist.router";
 import { superadminRouter } from "./v1.superadmin.router";
+import cors from "cors";
 
 export const apiRouterV1: Router = express.Router({ mergeParams: true });
+
+apiRouterV1.use(cors()); // CORS '*' : allow all cuz IP Whitelist is enabled
 
 apiRouterV1.use('/:ruid/player', playerRouter);
 apiRouterV1.use('/:ruid/banlist', banlistRouter);
