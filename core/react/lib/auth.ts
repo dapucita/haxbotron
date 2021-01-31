8b1bf5b98db0f2d13935c852d00d9a4fe459f17b
dapucita <dapucita@gmail.com>
2021-01-31 20:21:00 +0900

    remove useless

--- core/react/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import client from './client';
-
-export function login(username: string, password: string) {
-    client.post('/api/v1/auth', { username, password });
-}
-
-export function check() {
-    client.get('/api/auth');
-}
