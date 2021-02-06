ce0c8124ad76b848a8e8d1383d2d50d48fcdea58
dapucita <dapucita@gmail.com>
2021-02-06 23:00:51 +0900

    small fix

--- core/react/component/common/Alert.tsx
+++ core/react/component/common/Alert.tsx
@@ -6,6 +6,8 @@ usage: <Alert severity="error/warning/success/info">This is an error message!</A
 type Color = 'success' | 'info' | 'warning' | 'error';
 https://material-ui.com/components/snackbars/
 */
+export type AlertColor = 'success' | 'info' | 'warning' | 'error';
+
 export default function Alert(props: AlertProps) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
